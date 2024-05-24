931a5296d735f92760f01252496d287f394d183d
emastel <emma.steele.413@gmail.com>
2024-05-24 16:08:43 -0600

    GameData changed into a class

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,33 @@
+package server;
+
+import server.Handlers.Handler;
+import spark.*;
+
+public class Server {
+
+    private Handler handler;
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        Spark.delete("/db", Handler::clearRequest);
+        Spark.post("/user", Handler::registerRequest);
+        Spark.post("/session", Handler::loginRequest);
+        Spark.delete("/session", Handler::logoutRequest);
+        Spark.get("/game", Handler::listGamesRequest);
+        Spark.post("/game", Handler::createGameRequest);
+        Spark.put("/game", Handler::joinGameRequest);
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+
+}
