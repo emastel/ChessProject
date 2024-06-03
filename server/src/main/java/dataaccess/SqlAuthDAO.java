c821f0d020a92839c4f1c2b9729eb891e12cb56b
emastel <emma.steele.413@gmail.com>
2024-06-03 17:29:52 -0600

    Cleanup

---
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -0,0 +1,76 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.sql.SQLException;
+
+public class SqlAuthDAO {
+
+    public SqlAuthDAO() throws DataAccessException {
+        String[] createStatements = {"""
+            CREATE TABLE IF NOT EXISTS auths (
+            id int not null AUTO_INCREMENT,
+            token varchar(256) not null,
+            username varchar(256) not null,
+            PRIMARY KEY (id)
+            )
+            """};
+        DatabaseManager.configureDatabase(createStatements);
+    }
+
+    public void createAuth(AuthData input) throws SQLException{
+        try(var con = DatabaseManager.getConnection()) {
+            try(var preparedStatement = con.prepareStatement("INSERT INTO auths (token, username) VALUES (?,?)")) {
+                preparedStatement.setString(1, input.authToken());
+                preparedStatement.setString(2, input.username());
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    public String getUser(String token) throws SQLException{
+        try(var con = DatabaseManager.getConnection()) {
+            try(var preparedStatement = con.prepareStatement("SELECT id, token, username FROM auths WHERE token=?")) {
+                preparedStatement.setString(1, token);
+                try(var rs = preparedStatement.executeQuery()) {
+                    while(rs.next()) {
+                        return rs.getString("username");
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            throw new SQLException(e.getMessage());
+        }
+        return null;
+    }
+
+    public void clear() throws DataAccessException{
+        try(var con = DatabaseManager.getConnection()) {
+            try(var preparedStatement = con.prepareStatement("DELETE FROM auths")) {
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch(Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public void deleteAuth(String username, String token) throws SQLException, DataAccessException{
+        try(var con = DatabaseManager.getConnection()) {
+            try(var preparedStatement = con.prepareStatement("DELETE FROM auths WHERE username=? AND token=?")) {
+                preparedStatement.setString(1,username);
+                preparedStatement.setString(2,token);
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch(Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+}
